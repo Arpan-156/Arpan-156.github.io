4ca3e0a69e3fc20effec0c14e7ecb97245df69a3
unknown <arpanganguly30@gmail.com>
2022-04-16 17:22:57 +0530

    modified submit button of contact-me page

--- script.js
+++ script.js
@@ -127,3 +127,21 @@ $(document).ready(function(){
 //       }
 //     }
 //   });
+
+//button ANIMATION
+const button = document.querySelector('.button');
+const submit = document.querySelector('.submit');
+
+function toggleClass() {
+	this.classList.toggle('active');
+}
+
+function addClass() {
+	this.classList.add('finished');
+}
+
+button.addEventListener('click', toggleClass);
+button.addEventListener('transitionend', toggleClass);
+button.addEventListener('transitionend', addClass);
+
+/* loader */
